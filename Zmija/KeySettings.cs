b827930fc6daff455c8ee3c5dad7ca465d17c9f3
mirnana <imrovmirna@gmail.com>
2023-01-17 18:06:55 +0100

    tiny fix

--- Zmija/KeySettings.cs
+++ Zmija/KeySettings.cs
@@ -20,12 +20,12 @@ namespace Zmija
 
         private void izmijeni_Click_left(object sender, string e)
         {
-            ZmijaForm.settings.GoLeftKey = ((KeyChange)sender).key;
+            ZmijaForm.settings.GoLeftKey = e;
             foreach (Control c in panelLeft.Controls)
             {
                 if (c is KeyChange)
                 {
-                    ((KeyChange)c).key = ((KeyChange)sender).key;
+                    ((KeyChange)c).key = e;
                 }
             }
         }
@@ -37,12 +37,12 @@ namespace Zmija
 
         private void izmijeni_Click_right(object sender, string e)
         {
-            ZmijaForm.settings.GoRightKey = ((KeyChange)sender).key;
+            ZmijaForm.settings.GoRightKey = e;
             foreach (Control c in panelRight.Controls)
             {
                 if (c is KeyChange)
                 {
-                    ((KeyChange)c).key = ((KeyChange)sender).key;
+                    ((KeyChange)c).key = e;
                 }
             }
         }
@@ -54,12 +54,12 @@ namespace Zmija
 
         private void izmijeni_Click_up(object sender, string e)
         {
-            ZmijaForm.settings.GoUpKey = ((KeyChange)sender).key;
+            ZmijaForm.settings.GoUpKey = e;
             foreach (Control c in panelUp.Controls)
             {
                 if (c is KeyChange)
                 {
-                    ((KeyChange)c).key = ((KeyChange)sender).key;
+                    ((KeyChange)c).key = e;
                 }
             }
         }
@@ -71,12 +71,12 @@ namespace Zmija
 
         private void izmijeni_Click_down(object sender, string e)
         {
-            ZmijaForm.settings.GoDownKey = ((KeyChange)sender).key;
+            ZmijaForm.settings.GoDownKey = e;
             foreach (Control c in panelDown.Controls)
             {
                 if (c is KeyChange)
                 {
-                    ((KeyChange)c).key = ((KeyChange)sender).key;
+                    ((KeyChange)c).key = e;
                 }
             }
         }
@@ -88,7 +88,7 @@ namespace Zmija
 
         private void keyChange_keySettings_izmijeni_Click(object sender, string e)
         {
-            ZmijaForm.settings.SettingsKey = ((KeyChange)sender).key;
+            ZmijaForm.settings.SettingsKey = e;
         }
 
         private void keyChange_keySettings_Load(object sender, EventArgs e)
@@ -98,7 +98,7 @@ namespace Zmija
 
         private void keyChange_instructions_izmijeni_Click(object sender, string e)
         {
-            ZmijaForm.settings.InstructionsKey = ((KeyChange)sender).key;
+            ZmijaForm.settings.InstructionsKey = e;
         }
 
         private void keyChange_instructions_Load(object sender, EventArgs e)
@@ -108,7 +108,7 @@ namespace Zmija
 
         private void keyChange_close_izmijeni_Click(object sender, string e)
         {
-            ZmijaForm.settings.CloseKey = ((KeyChange)sender).key;
+            ZmijaForm.settings.CloseKey = e;
         }
 
         private void keyChange_close_Load(object sender, EventArgs e)
